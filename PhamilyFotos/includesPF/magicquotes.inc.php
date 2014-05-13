7189aa155c4968177719865ba900592cc410dfec
dcaprani <dcaprani@me.com>
2014-05-13 19:25:45 +0100

    Updates to Project Report plus some small CSS changes

--- PhamilyFotos/includesPF/magicquotes.inc.php
+++ PhamilyFotos/includesPF/magicquotes.inc.php
@@ -6,10 +6,8 @@
 			$value = is_array($value) ?
 				array_map('stripslashes_deep', $value) :
 				stripslashes($value);
-				
 				return $value;
-		}
-		
+		}			
 		$_POST = array_map('stripslashes_deep', $_POST);
 		$_GET = array_map('stripslashes_deep', $_GET);
 		$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
